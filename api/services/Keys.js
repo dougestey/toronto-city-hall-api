f4cea669ec39992bf018fca0bfea4a53129b3802
Doug Estey <doug@designcofounders.com>
2015-05-31 22:44:06 -0400

    update key handling

--- api/services/Keys.js
+++ api/services/Keys.js
@@ -1,7 +1,19 @@
 var fs = require('fs');
 
+var keysStore = {};
+
+try {
+  keysStore.namara = fs.readFileSync('keys/namara');
+} catch (e) {
+  if (e.code === 'ENOENT') {
+    keysStore.namara = null;
+  } else {
+    throw e;
+  }
+}
+
 var Keys = {
-  namara: fs.readFileSync('keys/namara'),
+  namara: keysStore.namara,
 }
 
 module.exports = Keys;
